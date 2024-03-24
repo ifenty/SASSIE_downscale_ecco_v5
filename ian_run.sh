7da9d0a388509458e9ee048773603f168e9f60ad
Ian Fenty <ifenty@gmail.com>
2024-03-24 00:49:12 +0000

    changed default scratch to /tmp in ian_run.sh

--- ian_run.sh
+++ ian_run.sh
@@ -15,6 +15,6 @@ declare -a arr=("ocean_state_2D_day_mean"
 for i in "${arr[@]}"
 do
    echo "$i $1 $2"
-   python generate-sassie-ecco-netcdfs-s3.py --root_filenames $i --root_s3_name s3://ecco-model-granules/SASSIE/N1/ --root_dest_s3_name s3://ecco-processed-data/SASSIE/N1/V1/HH/NETCDF/  --files_to_process $1 $2 --push_to_s3
+   python generate-sassie-ecco-netcdfs-s3.py --root_filenames $i --root_s3_name s3://ecco-model-granules/SASSIE/N1/ --root_dest_s3_name s3://ecco-processed-data/SASSIE/N1/V1/HH/NETCDF/  --files_to_process $1 $2 --push_to_s3 -l /tmp
 done
 
